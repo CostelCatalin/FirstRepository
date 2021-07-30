42d22872441de24df19c37eddcb5f618d6f9df52
Your NameCosteCatalin <you@example.comcostel.catalin.vasiliu@gmail.com>
2021-07-29 21:24:14 -0400

    This is my first BitBucket

--- src/com/test/testPushIt.java
+++ src/com/test/testPushIt.java
@@ -2,4 +2,18 @@ package com.test;
 
 public class testPushIt {
 
+//	Step1:	go in cmd
+//	Step2:	get project path
+//	Step3:	we nnned to navigate to project directory (folder)
+//	Step4:	cd <path to project>
+//	Steps5:	git status[to get status of files]	
+//	Step6: 	git add . [to add all file to stage]
+//	Step7:	git status [to get status of file for commit]
+//	Step8: git commit -m "message"
+//	Step9: git push -u origin main [to push change]
+	
+	
+	
+	
+	
 }
